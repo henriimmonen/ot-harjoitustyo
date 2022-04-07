6d1062696a3f16c3b891d35370cc2c4fe3da55e7
Henri Immonen <henri.immonen000@gmail.com>
2022-04-07 10:02:25 +0300

    testit pelleteille

--- src/tests/coordinates_test.py
+++ src/tests/coordinates_test.py
@@ -19,6 +19,7 @@ class TestLevel(unittest.TestCase):
     def setUp(self):
         self.level = Level(test_level, size)
         self.pacman = self.level.pacman
+        self.pellets = self.level.pellets
 
     def coordinates_match(self, sprite, x, y):
         self.assertEqual(sprite.rect.x, x)
@@ -38,3 +39,16 @@ class TestLevel(unittest.TestCase):
     def test_not_going_through_walls(self):
         self.level.move_pacman(x=-50)
         self.coordinates_match(self.pacman, size * 3, size * 2)
+
+    def test_pellets_disappear_when_eaten(self):
+        all_pellets = len(self.pellets)
+        self.level.move_pacman(y=50)
+        one_pellet_less = len(self.pellets)
+        self.assertLess(one_pellet_less, all_pellets)
+
+    def test_pellets_remain_same_if_nothing_to_eat(self):
+        self.level.move_pacman(y=50)
+        all_pellets = len(self.pellets)
+        self.level.move_pacman(y=-50)
+        pellets_after_moving = len(self.pellets)
+        self.assertEqual(pellets_after_moving, all_pellets)
