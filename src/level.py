81a35c5b15849f2e502de79e2de787189cb979f7
Henri Immonen <henri.immonen000@gmail.com>
2022-04-22 12:04:01 +0300

    alustava DFS-algoritmi toimii

--- src/level.py
+++ src/level.py
@@ -5,12 +5,15 @@ from sprites.floor import Floor
 from sprites.pellet import Pellet
 from sprites.powerpellet import PowerPellet
 from sprites.ghost import Ghost
-
+from collections import deque
 
 class Level:
     def __init__(self, level_map, cell_size):
         self.cell_size = cell_size
         self.pacman = None
+        self.pacman_coordinates = []
+        self.ghost_coordinates = []
+        self.level = level_map
         self.walls = pygame.sprite.Group()
         self.floors = pygame.sprite.Group()
         self.pellets = pygame.sprite.Group()
@@ -46,12 +49,14 @@ class Level:
 
                 elif level_map[height][width] == 3:
                     self.pacman = Pacman(normalized_x, normalized_y)
+                    self.pacman_coordinates = [width, height]
                     self.floors.add(Floor(normalized_x, normalized_y))
 
                 elif level_map[height][width] == 4:
                     if ghost_count == 1:
                         self.ghost = Ghost(1, normalized_x, normalized_y)
                         self.ghosts.add(self.ghost)
+                        self.ghost_coordinates = [width, height]
                         self.floors.add(Floor(normalized_x, normalized_y))
                         ghost_count += 1
 
@@ -102,10 +107,45 @@ class Level:
             return
 
         self.pacman.rect.move_ip(direction[0], direction[1])
+        new_coordinates = [direction[0] // self.cell_size, direction[1]//self.cell_size]
+        self.pacman_coordinates = [self.pacman_coordinates[0] + new_coordinates[0], self.pacman_coordinates[1] + new_coordinates[1]]
+        print(self.pacman_coordinates)
         if self.pacman_meets_ghost():
             self.lives -= 1
             return "dead"
         self.pacman_eats()
 
     def move_ghost(self):
-        self.ghost.rect.move_ip(-30,0)
+        path = self.bfs([self.ghost_coordinates[0], self.ghost_coordinates[1]], [self.pacman_coordinates[0], self.pacman_coordinates[1]])
+        next_cell = path[1]
+        x_dir = next_cell[0] - self.ghost_coordinates[0]
+        y_dir = next_cell[1] - self.ghost_coordinates[1]
+        self.ghost.rect.move_ip(x_dir*self.cell_size, y_dir*self.cell_size)
+        self.ghost_coordinates = [self.ghost_coordinates[0] + x_dir, self.ghost_coordinates[1] + y_dir]
+
+    def bfs(self, start, target):
+        q = deque()
+        visited = []
+        path = []
+        q.append(start)
+        while q:
+            current = q.popleft()
+            visited.append(current)
+
+            if current == target:
+                break
+            for neighbour in [[0,1], [0,-1], [1,0], [-1,0]]:
+                if neighbour[0]+current[0] >= 0 and neighbour[0]+current[0] < len(self.level[0]):
+                    if neighbour[1]+current[1] >= 0 and neighbour[1]+current[1] < len(self.level):
+                        next_cell = [neighbour[0]+current[0], neighbour[1]+current[1]]
+                        if next_cell not in visited:
+                            if self.level[next_cell[1]][next_cell[0]] != 1:
+                                q.append(next_cell)
+                                path.append({"Current":current, "Next":next_cell})
+        shortest = [target]
+        while target != start:
+            for step in path:
+                if step["Next"] == target:
+                    target = step["Current"]
+                    shortest.insert(0, step["Current"])
+        return shortest
