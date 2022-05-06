a1112c11587fef36968f89d3da64e82522ec9caf
Henri Immonen <henri.immonen000@gmail.com>
2022-05-06 14:05:07 +0300

    lisätty tietokannan toiminnallisuutta

--- src/ui/gameloop.py
+++ src/ui/gameloop.py
@@ -55,6 +55,11 @@ class Gameloop:
         while True:
             if self.handle_gameover_events() is False:
                 break
+            name_text = self.font.render(
+                self.user_text, False, (255, 255, 255))
+            self.screen.blit(name_text, self.input_box)
+            pygame.display.flip()
+            self.clock.tick(60)
 
     def initialize_starting_screen(self):
         self.screen.fill((0, 0, 0))
@@ -69,13 +74,13 @@ class Gameloop:
         self.screen.blit(logo_image, (100, 200))
         self.screen.blit(start_text, (100, 300))
         self.screen.blit(highscore_text, (170, 0))
-        self.get_highscores_from_db()
+        self._get_highscores_from_db()
         pygame.display.update()
 
     def initialize_gameloop(self):
         self.screen.fill((0, 0, 0))
         pygame.draw.rect(self.screen, (0, 0, 0), self.score_box)
-        self.update_lives()
+        self._update_lives()
         self.level.all_sprites.draw(self.screen)
         pygame.display.update()
 
@@ -85,8 +90,13 @@ class Gameloop:
             "GAME OVER", False, (190, 150, 100))
         score_text = self.font.render(
             f"SCORE: {self.level.score}", False, (107, 183, 210))
+        score_check = self._check_highscores_from_db()
+        if self.level.score > score_check:
+            self.input_box = pygame.Rect(190, 100, 100, 30)
+            self.user_text = ''
         self.screen.blit(score_text, (200, 0))
         self.screen.blit(gameover_text, (200, 200))
+        pygame.draw.rect(self.screen, (0, 0, 0), self.input_box)
         pygame.display.update()
 
     def handle_starting_events(self):
@@ -133,7 +143,7 @@ class Gameloop:
             if event.type == pygame.QUIT:
                 return False
 
-        self.update_round()
+        self._update_round()
 
         if self.check_collision():
             return False
@@ -149,15 +159,28 @@ class Gameloop:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     return False
+                if event.key == pygame.K_BACKSPACE:
+                    self.user_text = self.user_text[:-1]
+                    pygame.draw.rect(self.screen, (0, 0, 0), self.input_box)
+                if event.key == pygame.K_RETURN:
+                    self.enter_name_to_db()
+                    return False
+                else:
+                    self.user_text += event.unicode
             if event.type == pygame.QUIT:
                 return False
         return True
 
-    def get_highscores_from_db(self):
+    def _get_highscores_from_db(self):
         connection = sqlite3.connect('highscores.db')
         cur = connection.cursor()
-        connection.commit()
         y_coordinate = 2*CELL_SIZE
+        cur.execute(
+            'SELECT player, score FROM highscores ORDER BY score DESC LIMIT 3')
+        if cur.fetchall() == []:
+            no_scores_text = self.font.render("NO SCORES YET", False, (107, 183, 210))
+            self.screen.blit(no_scores_text, (170, y_coordinate))
+
         for player, score in cur.execute('SELECT player, score FROM highscores ORDER BY score DESC LIMIT 3'):
             player_score = player + ": " + str(score)
             highscores = self.font.render(player_score, False, (107, 183, 210))
@@ -165,28 +188,39 @@ class Gameloop:
             y_coordinate += CELL_SIZE
         connection.close()
 
-    def update_score(self):
-        """Päivitetään vasemman yläkulman pistetilanne.
-        """
+    def _check_highscores_from_db(self):
+        connection = sqlite3.connect('highscores.db')
+        cur = connection.cursor()
+        cur.execute('SELECT score FROM highscores ORDER BY score ASC LIMIT 3')
+        result = cur.fetchone()
+        connection.close()
+        if result == None:
+            return 0
+        return result[0]
+
+    def enter_name_to_db(self):
+        connection = sqlite3.connect('highscores.db')
+        cur = connection.cursor()
+        cur.execute("INSERT INTO highscores VALUES (?, ?)",
+                    (self.user_text, self.level.score))
+        connection.commit()
+        connection.close()
+
+    def _update_score(self):
         score_text = self.font.render(
             f"SCORE: {self.level.score}", False, (107, 183, 210))
         self.screen.fill((0, 0, 0), self.score_box)
         self.screen.blit(score_text, self.score_box)
 
-    def update_lives(self):
-        """Päivitetään oikean yläkulman elämät-näyttävä ruutu.
-        """
+    def _update_lives(self):
         lives_text = self.font.render(
             f"LIVES: {self.level.lives}", False, (107, 183, 210))
         self.screen.blit(lives_text, (400, 0))
 
-    def update_round(self):
-        """Päivitetään kierros. Liikutetaan haamut, liikutetaan pacman-sprite,
-        päivitetään pisteet ja tarkistetaan onko kaikki pelletit syöty.
-        """
+    def _update_round(self):
         self.move_ghosts()
         self.level.move_pacman(self.level.pacman.new_direction)
-        self.update_score()
+        self._update_score()
         pygame.display.update()
 
         if self.level.all_pellets_eaten():
@@ -206,7 +240,8 @@ class Gameloop:
         ja tarvittaessa aloitetaan uudelleen tai näytetään game over-ruutu.
 
         Returns:
-            _type_: _description_
+            True jos peli päättyy, False jos elämiä on jäljellä. None jos törmäystä
+            ei tapahtunut.
         """
         collision = self.level.pacman_meets_ghost()
         if collision and self.level.lives >= 0:
@@ -223,8 +258,8 @@ class Gameloop:
         """Aloitetaan uudelleen kun pacman-sprite menettää elämän.
         """
         self.screen.fill((0, 0, 0))
-        self.update_lives()
-        self.update_score()
+        self._update_lives()
+        self._update_score()
         self.level.position_pacman_and_ghosts_to_start()
         self.level.all_sprites.draw(self.screen)
         pygame.display.update()
@@ -236,4 +271,3 @@ class Gameloop:
         self.level.cleared += 1
         self.level.initialize_sprites()
         self.level.position_pacman_and_ghosts_to_start()
-
